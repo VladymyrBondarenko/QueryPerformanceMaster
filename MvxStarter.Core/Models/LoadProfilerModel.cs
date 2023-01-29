65c5667df233af60935fd697df5cde322c84b2ad
Бондаренко Владимир Валерьевич <vladymyr.bondarenko1@gmail.com>
2023-01-29 12:00:45 +0200

    Desktop UI. Init. Added Wpf Class Lib.

---
+++ MvxStarter.Core/Models/LoadProfilerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxStarter.Core.Models
+{
+    public class LoadProfilerModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
